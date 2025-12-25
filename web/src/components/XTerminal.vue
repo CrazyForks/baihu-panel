cc3a09c1c2ae8a373716edc11d2bb6050285368a
engigu <sayheya@qq.com>
2025-12-25 10:03:05 +0800

    feat: opt page style

--- web/src/components/XTerminal.vue
+++ web/src/components/XTerminal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, watch } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { Terminal } from '@xterm/xterm'
 import { FitAddon } from '@xterm/addon-fit'
 import '@xterm/xterm/css/xterm.css'
